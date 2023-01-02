d38d0f5de7758000bdb9a343d547083ba7c08cdf
reza_sandesta <sandesta24@gmail.com>
2023-01-02 16:33:45 +0700

    tambah portfolio lagi

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReceiptApp from '../assets/receiptApp.png'
 import SisfoSchool from '../assets/sisfoSchool.png'
 import whatsappBot from '../assets/whatsappBot.png'
+import todolistApp from '../assets/todolistApp.png'
 
 const Portfolio = () => {
   return (
@@ -11,7 +12,7 @@ const Portfolio = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-green-600'>Portfolio</p>
                 <p className='py-6'>Here are some of the projects I've worked on</p>
             </div>
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>                
+            <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-4'>                
                 <div style={{backgroundImage:`url(${ReceiptApp})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
@@ -43,6 +44,18 @@ const Portfolio = () => {
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="https://github.com/sandestareza/whatsapp-bot">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Github</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage:`url(${todolistApp})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Todo List App
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://todos-item.netlify.app">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Github</button>
                             </a>
                         </div>
